1bcdbbc1fd5b9ba9a9136e0505d3ad8a3df39805
Raul <rmingo@re7systems.com>
2023-09-22 12:26:31 +0200

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors= require("cors")
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port= process.env.PORT;
+        this.usuariosPath = "/api/usuarios";
+        this.midddlewares();
+
+        this.routes();
+    }
+
+    midddlewares(){
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use( express.static("public"));
+        
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require("../routes/usuarios"))
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log("Servidor corriendo en el puerto", this.port);
+        });
+    }
+
+}
+
+module.exports = Server;
